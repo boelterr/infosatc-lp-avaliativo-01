4ac57c577c573cbcd2da63b7ef66352363fb1a32
boelterr <bielboelter@gmail.com>
2021-09-02 14:02:12 -0300

    Create exercicio07.py
    Exercício 7

---
+++ exercicio07.py
@@ -0,0 +1,8 @@
+#Gabriel Custodio Boelter 2190
+
+F  = float(input("Digite um valor em Fahrenheit: ")) #Entrada da variavel F pedindo para lhe dar um valor
+
+
+C = (5.0*(F-32.0)/9)#Entrada da variavel C com sua equação correspondente
+
+print("O valor da conversão para Celcius é de {}" .format(C))#Print na tela mostrando o resultado
