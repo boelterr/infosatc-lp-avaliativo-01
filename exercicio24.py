3792758b0a19f4cea71e8de876ff4c78bd4ec1a7
boelterr <bielboelter@gmail.com>
2021-09-02 14:12:58 -0300

    Create exercicio24.py
    Exercício 24

---
+++ exercicio24.py
@@ -0,0 +1,7 @@
+#Gabriel Custodio Boelter 2190
+
+m = float(input("Digite um valor em m²: "))#Entrada da variavel m pedindo para lhe dar um valor
+
+a = (m/0.000247)#Entrada da variavel a com sua equação correspondente
+
+print("O valor da conversão para acres é {}" .format(a))#Print na tela mostrando o resultado
