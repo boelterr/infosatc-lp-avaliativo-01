768a00b9c18e0572ab5e2cc3f9242f48c9294495
boelterr <bielboelter@gmail.com>
2021-09-02 14:05:16 -0300

    Create exercicio13.py
    Exercício 13

---
+++ exercicio13.py
@@ -0,0 +1,7 @@
+#Gabriel Custodio Boelter 2190
+
+K = float(input("Digite um valor em km: "))#Entrada da variavel K pedindo para lhe dar um valor
+
+M = (K/1.61)#Entrada da variavel M com sua equação correspondente
+
+print("O valor da conversão para milhas é {}" .format(M))#Print na tela mostrando o resultado
