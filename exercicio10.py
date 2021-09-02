88d44f5e6e46f88175f092f40801ad4854343f75
boelterr <bielboelter@gmail.com>
2021-09-02 14:03:00 -0300

    Create exercicio10.py
    Exercício 10

---
+++ exercicio10.py
@@ -0,0 +1,7 @@
+#Gabriel Custodio Boelter 2190
+
+KM = float(input("Digite um valor em KM/h: "))#Entrada da variavel KM pedindo para lhe dar um valor
+
+MS = (KM/3.6)#Entrada da variavel MS com sua equação correspondente
+
+print("O valor da conversão para m/s é {}" .format(MS))#Print na tela mostrando o resultado
