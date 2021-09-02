bbb21cdc632cdbaf59ba4de32a2e9a58b2ea88b3
boelterr <bielboelter@gmail.com>
2021-09-02 14:11:19 -0300

    Create exercicio22.py
    Exercício 22

---
+++ exercicio22.py
@@ -0,0 +1,7 @@
+#Gabriel Custodio Boelter 2190
+
+j = float(input("Digite um valor em jardas: "))#Entrada da variavel j pedindo para lhe dar um valor
+
+m = (j*0.91)#Entrada da variavel m com sua equação correspondente
+
+print("O valor da conversão para metros é {}" .format(m))#Print na tela mostrando o resultado
