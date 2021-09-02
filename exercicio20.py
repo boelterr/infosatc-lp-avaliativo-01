004a658a456bd43f5cc1acf7b84f9f84f2c4a7cf
boelterr <bielboelter@gmail.com>
2021-09-02 14:09:30 -0300

    Create exercicio20.py
    Exercício 20

---
+++ exercicio20.py
@@ -0,0 +1,7 @@
+#Gabriel Custodio Boelter 2190
+
+k = float(input("Digite um valor em quilos: "))#Entrada da variavel k pedindo para lhe dar um valor
+
+l = (k/0.45)#Entrada da variavel l com sua equação correspondente
+
+print("O valor da conversão para libras é {}" .format(l))#Print na tela mostrando o resultado
