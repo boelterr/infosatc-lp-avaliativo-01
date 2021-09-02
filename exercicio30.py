dc15f344d9781aa30b5915f6107045da380863b7
boelterr <bielboelter@gmail.com>
2021-09-02 14:45:10 -0300

    Update exercicio30.py
    Exercício 30

---
+++ exercicio30.py
@@ -0,0 +1,7 @@
+#Gabriel Custodio Boelter 2190
+
+r = float(input("Digite um valor em Reais: "))#Entrada da variavel r pedindo para lhe dar um valor
+
+c = (r * 5.16)#Entrada da variavel c com sua equação correspondente
+
+print("O valor da conversão para Dolares é {}" .format(c))#Print na tela mostrando o resultado
