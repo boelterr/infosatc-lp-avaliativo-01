7db76f24f48fd6abf270e2d93dd91001810b738c
boelterr <bielboelter@gmail.com>
2021-09-02 14:25:51 -0300

    Create exercicio26.py
    Exercício 26

---
+++ exercicio26.py
@@ -0,0 +1,7 @@
+#Gabriel Custodio Boelter 2190
+
+m = float(input("Digite um valor em m²: "))#Entrada da variavel m pedindo para lhe dar um valor
+
+h = (m/0.0001)#Entrada da variavel h com sua equação correspondente
+
+print("O valor da conversão para hectares é {}" .format(h))#Print na tela mostrando o resultado
