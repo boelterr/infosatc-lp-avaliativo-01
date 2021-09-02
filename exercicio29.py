1f2b4640bd0dca9b1131a9f2cc58ae1cdbdb4ed7
boelterr <bielboelter@gmail.com>
2021-09-02 14:26:11 -0300

    Create exercicio29.py
    Exercício 29

---
+++ exercicio29.py
@@ -0,0 +1,10 @@
+#Gabriel Custodio Boelter 2190
+
+n1 = float(input("Digite a primeira nota do aluno: "))#Entrada das variaveis n1,n2,n3,4 pedindo para lhe dar um valor
+n2 = float(input("Digite a segunda nota do aluno: "))
+n3 = float(input("Digite a terceira nota do aluno: "))
+n4 = float(input("Digite a quarta nota do aluno: "))
+
+m = ((n1+n2+n3+n4)/4)#Entrada da variavel m com sua equação correspondente
+
+print("O valor da media do aluno é {}" .format(m))#Print na tela mostrando o resultado
