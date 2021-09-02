3ec652226174aa003946ebe677c2ac3dfee0d5a7
boelterr <bielboelter@gmail.com>
2021-09-02 14:09:00 -0300

    Create exercicio18.py
    Exercício 18

---
+++ exercicio18.py
@@ -0,0 +1,7 @@
+#Gabriel Custodio Boelter 2190
+
+m3 = float(input("Digite um valor em m³: "))#Entrada da variavel m3 pedindo para lhe dar um valor
+
+L = (1000*m3)#Entrada da variavel L com sua equação correspondente
+
+print("O valor da conversão para litros é {}" .format(L))#Print na tela mostrando o resultado
