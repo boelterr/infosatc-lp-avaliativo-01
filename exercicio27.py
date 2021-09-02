2431a22a1a0b2504706f0e186cd80857ee107192
boelterr <bielboelter@gmail.com>
2021-09-02 14:26:02 -0300

    Create exercicio27.py
    Exercício 27

---
+++ exercicio27.py
@@ -0,0 +1,7 @@
+#Gabriel Custodio Boelter 2190
+
+h = float(input("Digite um valor em hectares: "))#Entrada da variavel h pedindo para lhe dar um valor
+
+m = (h*10000)#Entrada da variavel m com sua equação correspondente
+
+print("O valor da conversão para m² é {}" .format(m))#Print na tela mostrando o resultado
